7f8eb70c725da20883c2095223fb309b005f789a
Luis Rodriguez <luisrdgzalm@gmail.com>
2025-12-03 12:39:31 -0600

    feat: literalObject

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -1,9 +1,14 @@
 // importación nombrada
 import { changeLogo } from "./modules/changeLogo.js";
 import { changePrincipalTitle } from "./modules/changePrincipalTitle.js";
+import { alicelUser } from "./modules/literalObject.js";
 
 changePrincipalTitle("title");
 
 //Crear una función que se le pase el nombre de la imagen a mostrar (cat o dino)
 //esta imagen es la que se debe mostrar en el elemento con id "logo"
 changeLogo("./cat.jpg");
+
+console.log( alicelUser.getFullName());
+console.log( alicelUser );
+console.log( alicelUser.stringify());
